9bb121b726e49fd68b1268198eb0030200e17a14
HIDX <abdulwahidkk123@gmail.com>
2019-05-16 02:07:14 +0700

    new

---
+++ app/src/main/java/com/hid/restorantq/ui/adapter/DetailKategoriAdapter.kt
@@ -0,0 +1,23 @@
+package com.hid.restorantq.ui.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.hid.restorantq.model.DetailKategori
+import com.hid.restorantq.ui.viewholder.DetailKategoriViewHolder
+
+class DetailKategoriAdapter(private val list: List<DetailKategori>, val clickListener: (DetailKategori) -> Unit): RecyclerView.Adapter<DetailKategoriViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailKategoriViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return DetailKategoriViewHolder(inflater, parent)
+    }
+
+    override fun onBindViewHolder(holder: DetailKategoriViewHolder, position: Int) {
+        val detailKategori: DetailKategori = list[position]
+        holder.bind(detailKategori, clickListener)
+    }
+
+    override fun getItemCount(): Int = list.size
+
+}
---
+++ app/src/main/java/com/hid/restorantq/ui/fragment/FragmentMilikSaya.kt
@@ -0,0 +1,21 @@
+package com.hid.restorantq.ui.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hid.restorantq.R
+
+class FragmentMilikSaya: Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var rootView = inflater!!.inflate(R.layout.fragment_milik_saya, container, false)
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view!!, savedInstanceState)
+
+    }
+}
